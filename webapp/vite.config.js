19a9f922d85318d22351cf7905336b820c740857
Diego Armando Pinta Cuasquen <diarpicu2022@gmail.com>
2026-04-13 12:34:07 -0500

    Corregir build para Vercel

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: { host: true, port: 3000 },
-  build:  { outDir: 'dist' }
+  build: { 
+    outDir: 'dist',
+    minify: 'esbuild',
+    sourcemap: false
+  }
 })
